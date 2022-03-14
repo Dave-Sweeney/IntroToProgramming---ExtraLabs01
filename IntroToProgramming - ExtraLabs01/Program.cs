3576339700a07c49948437b6fb4b1cbbf4e7c14f
David J. Sweeney <david.j.sweeney@outlook.com>
2022-03-14 17:29:09 -0400

    Initial commit.

--- IntroToProgramming - ExtraLabs01/Program.cs
+++ IntroToProgramming - ExtraLabs01/Program.cs
@@ -10,6 +10,52 @@ namespace IntroToProgramming___ExtraLabs01
     {
         static void Main(string[] args)
         {
+            int gridSize = 0;
+            bool validInput = false;
+
+            do
+            {
+                Console.WriteLine("How big do you want the grid?");
+                try
+                {
+                    gridSize = Int32.Parse(Console.ReadLine());
+                    validInput = true;
+                }
+                catch (Exception e)
+                {
+                    validInput = false;
+                    Console.WriteLine("Nope. Not a valid number. Try again!");
+                }
+            } while (!validInput);
+            
+            
+
+            GenerateGrid(gridSize);
+
+            Console.ReadKey();
         }
+
+        public static void GenerateGrid(int size)
+        {
+            
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("O");
+                    }
+                }
+                              
+                Console.WriteLine();
+            }
+        }
+
+
     }
 }
